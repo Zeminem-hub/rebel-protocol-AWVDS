a74b1132eef3400538eb67a6c12b43ecda3397e7
Mohammad Zeeshan Khan <zeeeminem@gmail.com>
2026-03-02 23:18:35 +0530

    Fix port to 8080 for Railway

--- app.py
+++ app.py
@@ -99,5 +99,5 @@ def scan():
 
 if __name__ == "__main__":
     import os
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(debug=False, host="0.0.0.0", port=port)
